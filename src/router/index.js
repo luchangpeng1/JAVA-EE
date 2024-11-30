a470ca0fc5c61e74054d6f802e399de4defc31e6
luchangpeng1 <2380567063@qq.com>
2024-11-30 16:54:44 +0800

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,33 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import ChoiceView from '../views/ChoiceView.vue'
+import ShortAnswerView from '../views/ShortAnswerView.vue'
+import ProgrammingView from '../views/ProgrammingView.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/choice'
+  },
+  {
+    path: '/choice',
+    name: 'choice',
+    component: ChoiceView
+  },
+  {
+    path: '/short-answer',
+    name: 'shortAnswer',
+    component: ShortAnswerView
+  },
+  {
+    path: '/programming',
+    name: 'programming',
+    component: ProgrammingView
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory('/JAVA-EE/'),
-  routes: [...]
+  routes
 })
 
 export default router
